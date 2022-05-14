acd5da6dccc66c9ccc7d41e9ef2a76e8bd1fe50f
Izabela <izabela.tarasin@gmail.com>
2022-05-14 13:23:46 +0200

    added simple example for jwt

---
+++ JWT_example/JWT_example/Migrations/20220513150448_initial2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JWT_example.Migrations
+{
+    public partial class initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "UserInfo");
+
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "HireDate",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "RowGuid",
+                table: "Employee");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "UserInfo",
+                type: "timestamp with time zone",
+                unicode: false,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Employee",
+                type: "timestamp with time zone",
+                unicode: false,
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HireDate",
+                table: "Employee",
+                type: "timestamp with time zone",
+                unicode: false,
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Employee",
+                type: "timestamp with time zone",
+                unicode: false,
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RowGuid",
+                table: "Employee",
+                type: "uuid",
+                unicode: false,
+                maxLength: 50,
+                nullable: true);
+        }
+    }
+}
---
+++ JWT_example/JWT_example/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace JWT_example.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string? NationalIDNumber { get; set; }
+        public string? EmployeeName { get; set; }
+        public string? LoginID { get; set; }
+        public string? JobTitle { get; set; }
+        public string? MaritalStatus { get; set; }
+        public string? Gender { get; set; }
+        public short VacationHours { get; set; }
+        public short SickLeaveHours { get; set; }
+    }
+}
+
